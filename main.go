1f822d83945757bed18947d1a73fc226e703e962
Student <nsalabugas@gmail.com>
2025-01-01 18:02:56 +0300

    Добавлены две структуры

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import "time"
+
+type Bin struct {
+	id        string
+	private   bool
+	createdAt time.Time
+	name      string
+}
+
+func newBin(id, name string, private bool) *Bin {
+	return &Bin{
+		id:        id,
+		name:      name,
+		private:   private,
+		createdAt: time.Now(),
+	}
+
+}
+
+type BinList struct {
+	bins []Bin
+}
+
+func (binList BinList) addBin(bin *Bin) {
+	binList.bins = append(binList.bins, *bin)
+}
+func main() {
+
+}
